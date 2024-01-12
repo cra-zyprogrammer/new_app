29cd57ff818bb6d417cbdf2925706dc3933d902a
Shubham Singh <yadavshubhamsingh00@gmail.com>
2024-01-12 20:07:52 +0530

    day 3 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:new_app/home_page.dart';
+import 'package:new_app/pages/home_page.dart';
+import 'package:new_app/pages/login_page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     int hrs = 8;
 
     return MaterialApp(
-      home: HomePage(),
+      themeMode: ThemeMode.light,
+      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+      ),
+      initialRoute: "/home",
+      routes: {
+        "/": (context) => LoginPage(),
+        "/home":(context) => HomePage(),
+        "/login":(context) => LoginPage()},
+
     );
   }
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,18 +3,24 @@ import 'package:flutter/material.dart';
 class HomePage extends StatelessWidget {
   final int hrs = 8;
   final String name = "Flutter";
+
+  const HomePage({super.key});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Catalog App"),
+        title: const Text(
+          "Catalog App",
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),),
       ),
       body: Center(
         child: Container(
           child: Text("TESTING $name FOR $hrs HOURS"),
         ),
       ),
-      drawer: Drawer(),
+      drawer: const Drawer(),
     );
   }
 }
